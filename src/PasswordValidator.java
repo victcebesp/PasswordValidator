38c7e75af389fc1a605cf0d74c8048fa44937b79
Victor Ceballos <victcebesp@gmail.com>
2016-04-25 20:05:36 +0100

    Refactor: Extract method

--- src/PasswordValidator.java
+++ src/PasswordValidator.java
@@ -1,5 +1,9 @@
 public class PasswordValidator {
     public static boolean validate(String password){
+        return hasMoreThanEightCharacters(password);
+    }
+
+    private static boolean hasMoreThanEightCharacters(String password) {
         return password.length() >= 8;
     }
 }
