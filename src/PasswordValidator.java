c4731f1b232d97735bfc82019cd1e0793a519a9a
Victor Ceballos <victcebesp@gmail.com>
2016-04-27 20:00:21 +0100

    Extract initialization to field

--- src/PasswordValidator.java
+++ src/PasswordValidator.java
@@ -1,15 +1,16 @@
 public class PasswordValidator {
 
     private static final LowerCaseRule lowerCaseRule = new LowerCaseRule();
-    private static LengthRule lengthRule = new LengthRule();
-    private static UnderScoreRule underScoreRule = new UnderScoreRule();
-    private static UpperCaseRule upperCaseRule = new UpperCaseRule();
+    private static final OneNumberRule oneNumberRule = new OneNumberRule();
+    private static final LengthRule lengthRule = new LengthRule();
+    private static final UnderScoreRule underScoreRule = new UnderScoreRule();
+    private static final UpperCaseRule upperCaseRule = new UpperCaseRule();
 
     public static boolean validate(String password){
         return  lengthRule.validate(password)
                 && underScoreRule.validate(password)
                 && upperCaseRule.validate(password)
                 && lowerCaseRule.validate(password)
-                && new OneNumberRule().validate(password);
+                && oneNumberRule.validate(password);
     }
-}//replace method with polimorphism
+}
